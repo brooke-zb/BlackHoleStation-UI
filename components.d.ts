b7fac5b5d201d99e224684b03ab386ad3b30ff7c
brooke_zb <brooke_zb@qq.com>
2022-02-28 21:03:38 +0800

    optimize global style

--- components.d.ts
+++ components.d.ts
@@ -4,11 +4,7 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
-    Background: typeof import('./src/components/page/Background.vue')['default']
-    Button: typeof import('./src/components/ui/button/Button.vue')['default']
-    Footer: typeof import('./src/components/page/Footer.vue')['default']
     IRegularBars: typeof import('~icons/regular/bars')['default']
-    IRegularCalendar: typeof import('~icons/regular/calendar')['default']
     IRegularCalendarLines: typeof import('~icons/regular/calendar-lines')['default']
     IRegularCalendarLinesPen: typeof import('~icons/regular/calendar-lines-pen')['default']
     IRegularChevronLeft: typeof import('~icons/regular/chevron-left')['default']
@@ -17,23 +13,12 @@ declare module 'vue' {
     IRegularChevronsRight: typeof import('~icons/regular/chevrons-right')['default']
     IRegularEye: typeof import('~icons/regular/eye')['default']
     IRegularFolder: typeof import('~icons/regular/folder')['default']
-    IRegularFolderBlank: typeof import('~icons/regular/folder-blank')['default']
     IRegularImage: typeof import('~icons/regular/image')['default']
     IRegularPlus: typeof import('~icons/regular/plus')['default']
-    IRegularSun: typeof import('~icons/regular/sun')['default']
     IRegularTag: typeof import('~icons/regular/tag')['default']
     ISolidAlien: typeof import('~icons/solid/alien')['default']
     ISolidBars: typeof import('~icons/solid/bars')['default']
-    Loading: typeof import('./src/components/page/Loading.vue')['default']
-    Menu: typeof import('./src/components/ui/menu/Menu.vue')['default']
-    MenuButton: typeof import('./src/components/ui/button/MenuButton.vue')['default']
-    MenuItem: typeof import('./src/components/ui/menu/MenuItem.vue')['default']
-    NavBar: typeof import('./src/components/page/NavBar.vue')['default']
-    Paginator: typeof import('./src/components/ui/paginator/Paginator.vue')['default']
-    PaginatorButton: typeof import('./src/components/ui/paginator/PaginatorButton.vue')['default']
-    PaginatorButtonGroup: typeof import('./src/components/ui/paginator/PaginatorButtonGroup.vue')['default']
-    Toast: typeof import('./src/components/ui/toast/Toast.vue')['default']
-    ToastContainer: typeof import('./src/components/ui/toast/ToastContainer.vue')['default']
+    ISolidXmark: typeof import('~icons/solid/xmark')['default']
   }
 }
 
