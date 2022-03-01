d0e0eab5f4a0c94b30c1a9cf652de38507597c6e
brooke_zb <brooke_zb@qq.com>
2022-03-02 01:40:22 +0800

    optimize gallery event

--- src/components/ui/gallery/GalleryContainer.vue
+++ src/components/ui/gallery/GalleryContainer.vue
@@ -10,17 +10,14 @@
           <div class="px-3 text-xs text-light-200 flex justify-center items-center">
             {{ currentIndex }} / {{ pictures.length }}
           </div>
-          <div @click="onClose"
+          <div @click="onGalleryClose"
                class="p-3 cursor-pointer fill-light-300 hover:fill-light-50 transition-colors">
             <ISolidXmark class="w-5 h-5"/>
           </div>
         </div>
 
         <!--图片-->
-        <div ref="overlay" class="picture-container flex justify-center items-center"
-             @mousedown.stop.prevent="onMouseDown" @mousemove.stop.prevent="onMouseMove"
-             @mouseup.stop.prevent="onMouseUp" @touchstart.stop.prevent="onTouchStart"
-             @touchmove.stop.prevent="onTouchMove" @touchend.stop.prevent="onTouchEnd">
+        <div ref="overlay" class="picture-container flex justify-center items-center">
           <img ref="image" :src="currentPicture" alt="" :class="classAppend"/>
         </div>
 
@@ -43,20 +40,11 @@ export default defineComponent({
 <script lang="ts" setup>
 import gsap from 'gsap'
 
-// 动画
 const isShow = ref(false)
 let InOutRatios = 1
 let InOutTranslateX = 0
 let InOutTranslateY = 0
 let refImage = ref<HTMLImageElement>()
-
-function onBeforeEnter(el: Element) {
-  document.addEventListener('keyup', preventKeyListener)
-  document.addEventListener('keydown', preventKeyListener)
-  gsap.set(el, {
-    '--tw-bg-opacity': 0,
-  })
-}
 
 const canZoom = ref(false)
 const isZooming = ref(false)
@@ -66,6 +54,13 @@ let maxTranslateDistance = reactive({
   x: 0,
   y: 0,
 })
+
+// 动画
+function onBeforeEnter(el: Element) {
+  gsap.set(el, {
+    '--tw-bg-opacity': 0,
+  })
+}
 
 function onEnter(el: Element, done: () => void) {
   resetData()
@@ -91,8 +86,6 @@ function onEnter(el: Element, done: () => void) {
 
 function onLeave(el: Element, done: () => void) {
   resetData()
-  document.removeEventListener('keyup', preventKeyListener)
-  document.removeEventListener('keydown', preventKeyListener)
   gsap.to(image.value, {
     scale: InOutRatios,
     translateX: InOutTranslateX,
@@ -124,267 +117,50 @@ const observer = new IntersectionObserver((entries) => {
 })
 
 function init(selector: string) {
-  // init container
   pictures.value = Array.from(document.querySelectorAll(selector))
   pictures.value.forEach((el, i) => {
     // init lazyload
     observer.observe(el)
 
+    // bind gallery event
     el.addEventListener('click', () => {
-      currentIndex.value = i + 1
-      currentPicture.value = el.src
-      currentAlt.value = el.alt
-      isShow.value = true
-      refImage.value = el
+      onGalleryEnter(el, i)
     })
   })
 }
 
-function onClose() {
+// 图片灯箱生命周期
+function onGalleryEnter(el: HTMLImageElement, i: number) {
+  if (el.complete) {
+    currentIndex.value = i + 1
+    currentPicture.value = el.src
+    currentAlt.value = el.alt
+    isShow.value = true
+    refImage.value = el
+
+    // 绑定灯箱事件
+    document.addEventListener('keyup', preventKeyListener)
+    document.addEventListener('keydown', preventKeyListener)
+    document.addEventListener('mousedown', onMouseDown)
+    document.addEventListener('mousemove', onMouseMove)
+    document.addEventListener('mouseup', onMouseUp)
+    document.addEventListener('touchstart', onTouchStart, { passive: false })
+    document.addEventListener('touchmove', onTouchMove, { passive: false })
+    document.addEventListener('touchend', onTouchEnd, { passive: false })
+  }
+}
+
+function onGalleryClose() {
   isShow.value = false
   resetData()
-}
-
-function onWheel(e: WheelEvent) {
-  currentZoomRatios.value -= e.deltaY * currentZoomRatios.value / 1000
-  computeMaxTranslate()
-  if (canZoom.value) {
-    // 放大
-    if (currentZoomRatios.value > 1 && currentZoomRatios.value <= maxZoomRatios.value) {
-      isZooming.value = true
-      gsap.to(image.value, {
-        scale: currentZoomRatios.value,
-        duration: 0.2,
-      })
-    }
-    // 放大超过最大值
-    else if (currentZoomRatios.value > maxZoomRatios.value) {
-      isZooming.value = true
-      gsap.to(image.value, {
-        scale: currentZoomRatios.value,
-        duration: 0.2,
-        onComplete: () => {
-          currentZoomRatios.value = maxZoomRatios.value
-          gsap.to(image.value, {
-            scale: currentZoomRatios.value,
-            duration: 0.2,
-          })
-        },
-      })
-    }
-    // 复原
-    else {
-      isZooming.value = false
-      gsap.to(image.value, {
-        scale: currentZoomRatios.value,
-        duration: 0.2,
-        onComplete: () => {
-          currentZoomRatios.value = 1
-          distance.x = 0
-          distance.y = 0
-          gsap.to(image.value, {
-            scale: currentZoomRatios.value,
-            translateX: 0,
-            translateY: 0,
-            duration: 0.2,
-          })
-        },
-      })
-    }
-  } else {
-    gsap.to(image.value, {
-      scale: currentZoomRatios.value,
-      duration: 0.2,
-      onComplete: () => {
-        currentZoomRatios.value = maxZoomRatios.value
-        gsap.to(image.value, {
-          scale: maxZoomRatios.value,
-          duration: 0.2,
-        })
-      },
-    })
-  }
-}
-
-const isDragging = ref(false)
-const startPos = reactive({
-  x: 0,
-  y: 0,
-})
-let lastPos = reactive({
-  x: 0,
-  y: 0,
-})
-const distance = reactive({
-  x: 0,
-  y: 0,
-})
-
-function onMouseDown(e: MouseEvent) {
-  if (e.button === 0) {
-    isDragging.value = true
-    startPos.x = e.clientX
-    startPos.y = e.clientY
-  }
-}
-
-function onMouseMove(e: MouseEvent) {
-  if (isDragging.value) {
-    // 缩放情况
-    if (isZooming.value) {
-      lastPos.x = e.clientX
-      lastPos.y = e.clientY
-      gsap.set(image.value, {
-        translateX: currentTranslate.value.x,
-        translateY: currentTranslate.value.y,
-      })
-    }
-  }
-}
-
-function onMouseUp(e: MouseEvent) {
-  if (e.button === 0) {
-    isDragging.value = false
-    // 非缩放情况
-    if (!isZooming.value) {
-      if ((e.target as Element)?.tagName === 'IMG') {
-        if (canZoom.value) {
-          isZooming.value = true
-          currentZoomRatios.value = maxZoomRatios.value
-          computeMaxTranslate()
-          gsap.to(image.value, {
-            scale: currentZoomRatios.value,
-            duration: 0.2,
-          })
-        }
-      } else {
-        onClose()
-      }
-    }
-    // 缩放情况
-    else {
-      // 移动距离小，取消缩放
-      if (Math.abs(e.clientX - startPos.x) + Math.abs(e.clientY - startPos.y) < 10) {
-        isZooming.value = false
-        distance.x = 0
-        distance.y = 0
-        gsap.to(image.value, {
-          scale: 1,
-          translateX: 0,
-          translateY: 0,
-          duration: 0.2,
-        })
-      }
-      // 计算移动距离
-      else {
-        lastPos.x = e.clientX
-        lastPos.y = e.clientY
-
-        computeDistance()
-        gsap.to(image.value, {
-          translateX: distance.x,
-          translateY: distance.y,
-          duration: 0.2,
-        })
-      }
-    }
-  }
-}
-
-let isPressing = false
-
-function onTouchStart(e: TouchEvent) {
-  if (e.touches.length === 1) {
-    isPressing = true
-    startPos.x = e.touches[0].clientX
-    startPos.y = e.touches[0].clientY
-    lastPos.x = e.touches[0].clientX
-    lastPos.y = e.touches[0].clientY
-  }
-}
-
-function onTouchMove(e: TouchEvent) {
-  if (isPressing) {
-    lastPos.x = e.touches[0].clientX
-    lastPos.y = e.touches[0].clientY
-
-    gsap.set(image.value, {
-      translateX: currentTranslate.value.x,
-      translateY: currentTranslate.value.y,
-    })
-  }
-}
-
-function onTouchEnd(e: TouchEvent) {
-  if (e.touches.length === 0) {
-    isPressing = false
-    // 非缩放情况
-    if (!isZooming.value) {
-      // 点击
-      if (Math.abs(lastPos.x - startPos.x) + Math.abs(lastPos.y - startPos.y) < 10) {
-        if ((e.target as Element)?.tagName === 'IMG') {
-          if (canZoom.value) {
-            isZooming.value = true
-            currentZoomRatios.value = maxZoomRatios.value
-            computeMaxTranslate()
-            gsap.to(image.value, {
-              scale: currentZoomRatios.value,
-              duration: 0.2,
-            })
-          }
-        } else {
-          onClose()
-        }
-      }
-      // 拖动
-      else if (Math.abs(lastPos.y - startPos.y) > 100) {
-        onClose()
-      } else {
-        gsap.to(image.value, {
-          translateX: 0,
-          translateY: 0,
-          duration: 0.2,
-        })
-      }
-    }
-    // 缩放情况
-    else {
-      // 点击，取消缩放
-      if (Math.abs(lastPos.x - startPos.x) + Math.abs(lastPos.y - startPos.y) < 10) {
-        isZooming.value = false
-        currentZoomRatios.value = 1
-        distance.x = 0
-        distance.y = 0
-        gsap.to(image.value, {
-          scale: currentZoomRatios.value,
-          translateX: 0,
-          translateY: 0,
-          duration: 0.2,
-        })
-      }
-      // 拖动，计算移动距离
-      else {
-        computeDistance()
-        gsap.to(image.value, {
-          translateX: distance.x,
-          translateY: distance.y,
-          duration: 0.2,
-        })
-      }
-    }
-  }
-}
-
-// 杂项
-const closeKeys = new Set(['Escape', 'Backspace', 'Delete'])
-const preventKeys = new Set(['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown', 'PageUp', 'PageDown', 'Home', 'End', ' '])
-function preventKeyListener(e: KeyboardEvent) {
-  if (closeKeys.has(e.key)) {
-    onClose()
-  }
-  if (preventKeys.has(e.key)) {
-    e.preventDefault()
-  }
+  document.removeEventListener('keyup', preventKeyListener)
+  document.removeEventListener('keydown', preventKeyListener)
+  document.removeEventListener('mousedown', onMouseDown)
+  document.removeEventListener('mousemove', onMouseMove)
+  document.removeEventListener('mouseup', onMouseUp)
+  document.removeEventListener('touchstart', onTouchStart)
+  document.removeEventListener('touchmove', onTouchMove)
+  document.removeEventListener('touchend', onTouchEnd)
 }
 
 function resetData() {
@@ -410,6 +186,257 @@ function resetData() {
   distance.y = 0
 }
 
+function onWheel(e: WheelEvent) {
+  currentZoomRatios.value -= e.deltaY * currentZoomRatios.value / 1000
+  computeMaxTranslate()
+  if (canZoom.value) {
+    // 放大
+    if (currentZoomRatios.value > 1 && currentZoomRatios.value <= maxZoomRatios.value) {
+      isZooming.value = true
+      gsap.to(image.value, {
+        scale: currentZoomRatios.value,
+        duration: 0.2,
+      })
+    }
+    // 放大超过最大值
+    else if (currentZoomRatios.value > maxZoomRatios.value) {
+      isZooming.value = true
+
+      // 限制缩放
+      currentZoomRatios.value = maxZoomRatios.value + (currentZoomRatios.value - maxZoomRatios.value) * 0.2
+      gsap.to(image.value, {
+        scale: currentZoomRatios.value,
+        duration: 0.2,
+        onComplete: () => {
+          currentZoomRatios.value = maxZoomRatios.value
+          gsap.to(image.value, {
+            scale: currentZoomRatios.value,
+            duration: 0.2,
+          })
+        },
+      })
+    }
+    // 复原
+    else {
+      isZooming.value = false
+
+      // 限制缩放
+      currentZoomRatios.value = 1 - (1 - currentZoomRatios.value) * 0.2
+      gsap.to(image.value, {
+        scale: currentZoomRatios.value,
+        duration: 0.2,
+        onComplete: () => {
+          currentZoomRatios.value = 1
+          distance.x = 0
+          distance.y = 0
+          gsap.to(image.value, {
+            scale: currentZoomRatios.value,
+            translateX: 0,
+            translateY: 0,
+            duration: 0.2,
+          })
+        },
+      })
+    }
+  } else {
+
+    // 限制缩放
+    currentZoomRatios.value = currentZoomRatios.value > 1 ? 1 + (currentZoomRatios.value - 1) * 0.2 : currentZoomRatios.value
+    currentZoomRatios.value = currentZoomRatios.value < 1 ? 1 - (1 - currentZoomRatios.value) * 0.2 : currentZoomRatios.value
+    gsap.to(image.value, {
+      scale: currentZoomRatios.value,
+      duration: 0.2,
+      onComplete: () => {
+        currentZoomRatios.value = 1
+        gsap.to(image.value, {
+          scale: maxZoomRatios.value,
+          duration: 0.2,
+        })
+      },
+    })
+  }
+}
+
+const isDragging = ref(false)
+const startElement = ref<Element>()
+const startPos = reactive({
+  x: 0,
+  y: 0,
+})
+let lastPos = reactive({
+  x: 0,
+  y: 0,
+})
+const distance = reactive({
+  x: 0,
+  y: 0,
+})
+
+function onMouseDown(e: MouseEvent) {
+  e.stopPropagation()
+  e.preventDefault()
+  if (e.button === 0) {
+    isDragging.value = true
+    startElement.value = e.target as Element
+
+    startPos.x = e.clientX
+    startPos.y = e.clientY
+    lastPos.x = e.clientX
+    lastPos.y = e.clientY
+  }
+}
+
+function onMouseMove(e: MouseEvent) {
+  e.stopPropagation()
+  e.preventDefault()
+  if (isDragging.value) {
+    // 缩放情况
+    lastPos.x = e.clientX
+    lastPos.y = e.clientY
+
+    movePointerHandler()
+  }
+}
+
+function onMouseUp(e: MouseEvent) {
+  e.stopPropagation()
+  e.preventDefault()
+  if (e.button === 0) {
+    ReleasePointerHandler()
+  }
+}
+
+function onTouchStart(e: TouchEvent) {
+  e.stopPropagation()
+  e.preventDefault()
+  if (e.touches.length === 1) {
+    isDragging.value = true
+    startElement.value = e.target as Element
+
+    startPos.x = e.touches[0].clientX
+    startPos.y = e.touches[0].clientY
+    lastPos.x = e.touches[0].clientX
+    lastPos.y = e.touches[0].clientY
+  }
+}
+
+function onTouchMove(e: TouchEvent) {
+  e.stopPropagation()
+  e.preventDefault()
+  if (isDragging.value) {
+    lastPos.x = e.touches[0].clientX
+    lastPos.y = e.touches[0].clientY
+
+    movePointerHandler()
+  }
+}
+
+function onTouchEnd(e: TouchEvent) {
+  e.stopPropagation()
+  e.preventDefault()
+  if (e.touches.length === 0) {
+    ReleasePointerHandler()
+  }
+}
+
+function movePointerHandler() {
+  if (isZooming.value) {
+    gsap.set(image.value, {
+      translateX: currentTranslate.value.x,
+      translateY: currentTranslate.value.y,
+    })
+  } else {
+    gsap.set(image.value, {
+      translateX: (lastPos.x - startPos.x) * 0.2,
+      translateY: lastPos.y - startPos.y,
+    })
+  }
+}
+
+function ReleasePointerHandler() {
+  // 非缩放情况
+  if (!isZooming.value) {
+    // 到达关闭临界点
+    if (isDragToClose()) {
+      onGalleryClose()
+    } else {
+      // 始于其他区域
+      if (startElement.value?.tagName !== 'IMG') {
+        if (isOnlyClick()) {
+          onGalleryClose()
+        } else {
+          gsap.to(image.value, {
+            translateX: 0,
+            translateY: 0,
+            duration: 0.2,
+          })
+        }
+      }
+      // 始于图片
+      else {
+        // 满足缩放条件
+        if (isOnlyClick() && canZoom.value) {
+          isZooming.value = true
+          currentZoomRatios.value = maxZoomRatios.value
+          computeMaxTranslate()
+          gsap.to(image.value, {
+            scale: currentZoomRatios.value,
+            translateX: 0,
+            translateY: 0,
+            duration: 0.2,
+          })
+        } else {
+          gsap.to(image.value, {
+            translateX: 0,
+            translateY: 0,
+            duration: 0.2,
+          })
+        }
+      }
+    }
+  }
+  // 缩放情况
+  else {
+    // 取消缩放
+    if (isOnlyClick()) {
+      isZooming.value = false
+      currentZoomRatios.value = 1
+      distance.x = 0
+      distance.y = 0
+      gsap.to(image.value, {
+        scale: currentZoomRatios.value,
+        translateX: 0,
+        translateY: 0,
+        duration: 0.2,
+      })
+    }
+    // 计算移动距离
+    else {
+      computeDistance()
+      gsap.to(image.value, {
+        translateX: distance.x,
+        translateY: distance.y,
+        duration: 0.2,
+      })
+    }
+  }
+  isDragging.value = false
+  startElement.value = undefined
+}
+
+// 杂项
+const closeKeys = new Set(['Escape', 'Backspace', 'Delete'])
+const preventKeys = new Set(['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown', 'PageUp', 'PageDown', 'Home', 'End', ' '])
+
+function preventKeyListener(e: KeyboardEvent) {
+  if (closeKeys.has(e.key)) {
+    onGalleryClose()
+  }
+  if (preventKeys.has(e.key)) {
+    e.preventDefault()
+  }
+}
+
 const currentTranslate = computed(() => {
   let x = distance.x + lastPos.x - startPos.x
   let y = distance.y + lastPos.y - startPos.y
@@ -430,8 +457,10 @@ const currentTranslate = computed(() => {
 })
 
 function computeMaxTranslate() {
-  maxTranslateDistance.x = (image.value.naturalWidth - image.value.width) / 2
-  maxTranslateDistance.y = (image.value.naturalHeight - image.value.height) / 2
+  let x = image.value.naturalWidth - document.documentElement.clientWidth
+  let y = image.value.naturalHeight - document.documentElement.clientHeight
+  maxTranslateDistance.x = x > 0 ? x / 2 : 0
+  maxTranslateDistance.y = y > 0 ? y / 2 : 0
 }
 
 function computeDistance() {
@@ -447,6 +476,14 @@ function computeDistance() {
   } else if (currentTranslate.value.y < -maxTranslateDistance.y) {
     distance.y = -maxTranslateDistance.y
   }
+}
+
+function isOnlyClick() {
+  return Math.abs(lastPos.x - startPos.x) + Math.abs(lastPos.y - startPos.y) < 6
+}
+
+function isDragToClose() {
+  return Math.abs(lastPos.y - startPos.y) > 100
 }
 
 // 图片灯箱功能
