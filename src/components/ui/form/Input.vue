c4be72e656abe52b6ed8720c055b9eafc6d4f5fb
brooke_zb <brooke_zb@qq.com>
2022-03-06 14:11:44 +0800

    optimize form component style

--- src/components/ui/form/Input.vue
+++ src/components/ui/form/Input.vue
@@ -4,7 +4,7 @@
       <slot name="left"></slot>
     </div>
     <input class="rounded-md outline-0 bg-transparent px-2 py-1.5 w-full border-2 placeholder:text-sm
-           border-light-300 dark:border-dark-600 focus:border-secondary-500 dark:focus:border-info-500"
+           border-light-300 dark:border-dark-600 focus:border-secondary-500 dark:focus:border-info-500 transition-bg"
            :class="classAppend" :type="props.type" :placeholder="props.placeholder" :value="props.modelValue" @input="onInput"/>
     <div v-if="$slots.right" class="absolute w-5 h-5 right-3 top-0 bottom-0 m-auto
     fill-light-400 dark:fill-dark-500" :class="slotClassAppend">
