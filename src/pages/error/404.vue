096ab9fcb532047966ed7edf99d7572f80fa2e24
brooke_zb <brooke_zb@qq.com>
2022-03-06 22:59:37 +0800

    optimize global style

--- src/pages/error/404.vue
+++ src/pages/error/404.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="p-2 flex justify-center">
-    <div class="flex flex-col space-y-4 rounded-lg bg-light-50/80 dark:bg-dark-700/80 p-8 backdrop-blur">
+  <div class="box-center p-2 flex justify-center">
+    <div class="flex flex-col gap-y-4 rounded-lg bg-light-50/80 dark:bg-dark-700/90 p-8 backdrop-blur">
       <h3 class="text-6xl font-bold text-danger-600 dark:text-danger-500">404</h3>
       <h4 class="text-4xl">页面丢失了</h4>
       <p>您要的页面已经消失在{{ isCurrentDarkMode ? '黑洞' : '星际' }}中了，可以尝试：</p>
