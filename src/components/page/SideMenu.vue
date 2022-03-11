a8bdf4c3c38fa573ae51f0de52d1940014363170
brooke_zb <brooke_zb@qq.com>
2022-03-11 14:42:02 +0800

    complete sidemenu

--- src/components/page/SideMenu.vue
+++ src/components/page/SideMenu.vue
@@ -11,62 +11,56 @@
           </div>
           <div class="px-2 py-1 text-sm select-none">{{ siteName }}</div>
           <div class="flex items-center px-3 cursor-pointer group" @click="store.toggleSideMenu">
-            <ISolidXmark class="group-hover:scale-150 group-hover:fill-danger-600 transition-all"/>
+            <ISolidXmark class="group-hover:scale-125 group-hover:fill-danger-600 transition-all"/>
           </div>
         </div>
         <!--文件栏-->
         <TabView :navs="tabItems" :active="activeIndex" @change="navigateTab">
-          <div class="p-4 gap-8 flex flex-col items-center justify-around">
-            <router-link class="link grow" to="/">
-              <IRegularHouse class="inline"/>
-              主页
-            </router-link>
-            <router-link class="link" to="/archives">
-              <IRegularBoxArchive class="inline"/>
-              归档
-            </router-link>
-            <router-link class="link" to="/categories">
-              <IRegularFolders class="inline"/>
-              分类
-            </router-link>
-            <router-link class="link" to="/tags">
-              <IRegularTags class="inline"/>
-              标签
-            </router-link>
-            <router-link class="link" to="/friends">
-              <IRegularLink class="inline"/>
-              友链
-            </router-link>
-          </div>
-          <div class="p-4 gap-8 flex flex-col sm:flex-row items-center sm:items-start">
-            <div>
-              <img :src="avatar" width="128" height="128" alt=""
-                   class="rounded-xl ring ring-light-400 dark:ring-dark-400 w-24 h-24 sm:w-36 sm:h-36"/>
-            </div>
-            <div>
-              <h2 class="text-xl text-secondary-500 dark:text-info-400 font-bold tracking-wide mb-1">关于本站</h2>
-              <p class="mb-1">一个分享技术日常的博客，偶尔记录生活。</p>
-              <p class="mb-4">博客系统和主题是自己编写的，自用为主。</p>
-              <h2 class="text-xl text-secondary-500 dark:text-info-400 font-bold tracking-wide mb-1">关于博主</h2>
-              <p class="mb-1">24岁，是学生。</p>
-              <p class="mb-1">码农，半桶水美工。</p>
-              <p class="mb-4 italic">尽力做自己感兴趣之事。</p>
-              <div class="flex items-center text-xl gap-4">
-                <a href="https://github.com/brooke-zb" target="_blank">
-                  <IBrandsGithub class="fill-light-600 dark:fill-light-100 w-5"/>
-                </a>
-                <a href="https://space.bilibili.com/14910487" target="_blank">
-                  <IBrandsBilibili class="fill-sky-400 w-5"/>
-                </a>
-                <a href="mailto:me@brookezb.com">
-                  <IRegularEnvelope class="fill-primary-400 w-5"/>
-                </a>
-                <a href="https://steamcommunity.com/id/brooke-zb" target="_blank">
-                  <IBrandsSteam class="fill-blue-900 dark:fill-light-100 w-5"/>
-                </a>
+          <TabPanel>
+            <div class="p-4 flex flex-col">
+              <div class="py-10 flex justify-center items-center grow text-4xl font-bold text-secondary-600 dark:text-info-400 select-none">
+                {{ currentTime }}
+              </div>
+              <div class="flex flex-col items-center gap-4">
+                <router-link v-for="item in navItems" :to="item.to"
+                             class="text-center w-1/2 transition-colors px-8 py-1 hover:bg-secondary-500 dark:hover:bg-info-500
+                             hover:text-light-50 dark:hover:text-light-50 hover:fill-light-50 dark:hover:fill-light-50"
+                             :class="{'text-secondary-500 dark:text-info-300 fill-secondary-500 dark:fill-info-300': item.to === $route.path,
+                             'text-light-900 dark:text-light-50 fill-light-900 dark:fill-light-50': item.to !== $route.path}">
+                  <component :is="item.icon" class="inline align-text-bottom"/>
+                  {{ item.label }}
+                </router-link>
               </div>
             </div>
-          </div>
+          </TabPanel>
+          <TabPanel>
+            <div class="p-4 gap-8 flex flex-col sm:flex-row items-center sm:items-start">
+              <div>
+                <img :src="avatar" width="128" height="128" alt=""
+                     class="rounded-xl ring ring-light-400 dark:ring-dark-400 w-24 h-24 sm:w-36 sm:h-36"/>
+              </div>
+              <div>
+                <h2 class="text-xl text-secondary-500 dark:text-info-400 font-bold tracking-wide mb-1">关于本站</h2>
+                <p class="mb-1">一个分享技术日常的博客，偶尔记录生活。</p>
+                <p class="mb-4">博客系统和主题是自己编写的，自用为主。</p>
+                <h2 class="text-xl text-secondary-500 dark:text-info-400 font-bold tracking-wide mb-1">关于博主</h2>
+                <p class="mb-1">24岁，是学生。</p>
+                <p class="mb-1">码农，半桶水美工。</p>
+                <p class="mb-4 italic">尽力做自己感兴趣之事。</p>
+                <div class="flex items-center text-xl gap-4">
+                  <a href="https://github.com/brooke-zb" target="_blank">
+                    <IBrandsGithub class="fill-light-600 dark:fill-light-100 w-5"/>
+                  </a>
+                  <a href="https://space.bilibili.com/14910487" target="_blank">
+                    <IBrandsBilibili class="fill-sky-400 w-5"/>
+                  </a>
+                  <a href="mailto:me@brookezb.com">
+                    <IRegularEnvelope class="fill-success-500 w-5"/>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </TabPanel>
         </TabView>
       </div>
     </div>
@@ -84,8 +78,14 @@ import store from '@/utils/store'
 import gsap from 'gsap'
 import avatar from '@/assets/img/avatar.png?url'
 import TabView from '@/components/ui/tabview/TabView.vue'
+import TabPanel from '@/components/ui/tabview/TabPanel.vue'
 import IRegularCompass from '~icons/regular/compass'
 import IRegularUser from '~icons/regular/user'
+import IRegularHouse from '~icons/regular/house'
+import IRegularBoxArchive from '~icons/regular/box-archive'
+import IRegularFolders from '~icons/regular/folders'
+import IRegularTags from '~icons/regular/tags'
+import IRegularLink from '~icons/regular/link'
 
 const siteName = import.meta.env.BHS_SITE_NAME
 const menu = ref()
@@ -127,6 +127,7 @@ function onLeave(el: Element, done: () => void) {
   })
 }
 
+// 菜单项
 const activeIndex = ref(0)
 const tabItems = [
   {
@@ -142,4 +143,37 @@ const tabItems = [
 function navigateTab(index: number) {
   activeIndex.value = index
 }
+
+// 导航项
+const now = useNow()
+const currentTime = computed(() => {
+  return now.value.toLocaleTimeString()
+})
+const navItems = [
+  {
+    label: '首页',
+    icon: IRegularHouse,
+    to: '/',
+  },
+  {
+    label: '归档',
+    icon: IRegularBoxArchive,
+    to: '/archive',
+  },
+  {
+    label: '分类',
+    icon: IRegularFolders,
+    to: '/categories',
+  },
+  {
+    label: '标签',
+    icon: IRegularTags,
+    to: '/tags',
+  },
+  {
+    label: '友链',
+    icon: IRegularLink,
+    to: '/friends',
+  },
+]
 </script>
