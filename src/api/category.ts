467ee6f9b167788f6e2175f32590987c438583a1
brooke_zb <brooke_zb@qq.com>
2022-03-28 22:03:40 +0800

    update api

--- src/api/category.ts
+++ src/api/category.ts
@@ -1,6 +1,9 @@
 import request from '@/utils/request'
 
 export default {
+  getByCid(cid: number) {
+    return request.get<BhsCategory>(`/category/${cid}`)
+  },
   getList() {
     return request.get<BhsCategory[]>('/category')
   }
--- src/api/tag.ts
+++ src/api/tag.ts
@@ -1,6 +1,9 @@
 import request from '@/utils/request'
 
 export default {
+  getByName(name: string) {
+    return request.get<Page<BhsArticle>>(`/tag/${name}`)
+  },
   getList() {
     return request.get<BhsTag[]>('/tag')
   }
